fdf323b042cc454113e9ae1e72432b5b0603199b
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2019-02-11 16:23:39 +0800

    app.js详细注释说明

--- nodejs-express/app.js
+++ nodejs-express/app.js
@@ -5,21 +5,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// 加载路由控制
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// 创建项目实例
 var app = express();
 
 // view engine setup
+// 定义EJS模板引擎和模板文件位置，也可以使用jade或其他模型引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// 定义日志和输出级别
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// 定义cookie解析器
 app.use(cookieParser());
+// 定义静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 匹配路径和路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -35,8 +42,10 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  // 生产环境，500错误处理
   res.status(err.status || 500);
   res.render('error');
 });
 
+// 输出模型app
 module.exports = app;
